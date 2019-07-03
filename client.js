448c54308e090534f43b96188f27a1d2a0b9ce26
JiadanWang <jiadanwang@gmail.com>
2019-07-03 22:23:59 +0000

    initial name

--- client.js
+++ client.js
@@ -3,12 +3,16 @@ const connect = function() {
   const conn = net.createConnection({ 
     host: '192.168.88.218',
     port: 50541
-  });conn.setEncoding('utf8'); 
-conn.on('data', (data) => {
-  console.log('Server says: ', data);
+  });
+  conn.setEncoding('utf8'); 
+  conn.on('data', (data) => {
+  console.log('Server says: '+ data);
 });
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
+  conn.on('connect', () => {
+    conn.write('Name: JD' );
+  });
+  
 
   return conn;
   
